5158d72fe8792f3ad065d401ffc7e87634d023e8
riyanqadiir <rayyanbhatti@gmail.com>
2024-08-21 04:27:37 +0500

    made chnages to the layout

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,17 @@ import { useNavigate } from 'react-router-dom';
 export default function Home() {
     const navigate = useNavigate();
     const handleCLick = (mode) => {
-            navigate(`/game/${mode}-game`);
+        navigate(`/game/${mode}-game`);
     }
     return (
-        <div className='home'>
-            <h1 className='text-center mt-5 text-danger'>Welcome to Tic-Tac-Toe Game</h1>
-            <div className="home-buttons mt-5 d-flex justify-content-center align-items-center h-100 flex-column gap-3">
-                <button className='btn btn-primary px-5 py-3'onClick={() => handleCLick('single')}>Single Player</button>
-                <button className='btn btn-danger px-5 py-3' onClick={() => handleCLick('multi')}>Multi Player</button>
+        <>
+            <h1 className='text-center text-danger'>Welcome to Tic-Tac-Toe Game</h1>
+            <div className='home'>
+                <div className="home-buttons d-flex justify-content-center align-items-center flex-column gap-3">
+                    <button className='btn btn-primary px-5 py-3' onClick={() => handleCLick('single')}>Single Player</button>
+                    <button className='btn btn-danger px-5 py-3' onClick={() => handleCLick('multi')}>Multi Player</button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
